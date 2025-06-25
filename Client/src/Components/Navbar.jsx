145fead48f41cc9b5db1937101a9693370c38ed5
Lakshaysehagl01 <sehgall333@gmail.com>
2025-06-25 18:16:43 +0530

    Added navbar and sidebar components, integrated wallet connection

---
+++ Client/src/Components/Navbar.jsx
@@ -0,0 +1,126 @@
+import { Link, useNavigate } from "react-router-dom";
+import { logo, menu, search, thirdweb } from "../assets";
+import { navlinks } from "../Constants";
+import { useState } from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
+function Navbar() {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState("dashboard");
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+  const { address } = useAccount();
+  const handleClick = () => {
+    if (address) navigate("/create-campaign");
+  };
+  return (
+    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
+      <div className="lg:flex-1 py-2 pl-4 pr-2 flex flex-row max-w-[458px] h-[52px] bg-[#1c1c24]">
+        <input
+          type="text"
+          placeholder="Search for campaigns"
+          className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none rounded-[100px]"
+        />
+        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
+          <img
+            src={search}
+            alt="search"
+            className="w-[15px] h-[15px] object-contain"
+          />
+        </div>
+      </div>
+      <div className="sm:flex hidden flex-row justify-end gap-4">
+        {address && (
+          <button
+            type="button"
+            className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] bg-[#8c6dfd]`}
+            onClick={handleClick}
+          >
+            Create Campaign
+          </button>
+        )}
+        <ConnectButton showBalance={false} />
+        <Link to={"/profile"}>
+          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+            <img
+              src={thirdweb}
+              alt="user"
+              className="w-[60%] h-[60%] object-contain"
+            />
+          </div>
+        </Link>
+      </div>
+
+      {/* Small screen navigation */}
+      <div className="sm:hidden flex justify-between items-center relative">
+        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+          <img
+            src={logo}
+            alt="user"
+            className="w-[60%] h-[60%] object-contain"
+          />
+        </div>
+
+        <img
+          src={menu}
+          alt="menu"
+          className="w-[34px] h-[34px] object-contain cursor-pointer"
+          onClick={() => setToggleDrawer((prev) => !prev)}
+        />
+
+        <div
+          className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${
+            !toggleDrawer ? "-translate-y-[100vh]" : "translate-y-0"
+          } transition-all duration-700`}
+        >
+          <ul className="mb-4">
+            {navlinks.map((link) => (
+              <li
+                key={link.name}
+                className={`flex p-4 ${
+                  isActive === link.name && "bg-[#3a3a43]"
+                }`}
+                onClick={() => {
+                  setIsActive(link.name);
+                  setToggleDrawer(false);
+                  navigate(link.link);
+                }}
+              >
+                <img
+                  src={link.imgUrl}
+                  alt={link.name}
+                  className={`w-[24px] h-[24px] object-contain ${
+                    isActive === link.name ? "grayscale-0" : "grayscale"
+                  }`}
+                />
+                <p
+                  className={`ml-[20px] font-epilogue font-semibold text-[14px] ${
+                    isActive === link.name ? "text-[#1dc071]" : "text-[#808191]"
+                  }`}
+                >
+                  {link.name}
+                </p>
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex mx-4 mb-4">
+            {address && (
+              <button
+                type="button"
+                className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] bg-[#8c6dfd]`}
+                onClick={handleClick}
+              >
+                Create Campaign
+              </button>
+            )}
+          </div>
+          <div className="flex mx-4">
+            <ConnectButton showBalance={false} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
